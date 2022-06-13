a0210d1a3999599fa6d8a332bd49cc849c1d4d4c
Oyinlade Ojesanmi <Ojesanmioyinlade@gmail.com>
2022-06-13 20:02:36 +0100

    update the javascricpt togglr

--- script.js
+++ script.js
@@ -4,9 +4,18 @@ const closeBar = document.getElementById('menu-close');
 const navPort =  document.getElementById('nav-port');
 const navAbout =  document.getElementById('nav-about');
 const navContact =  document.getElementById('nav-contact');
+const fixed = document.querySelector('.global');
 
 harmburger.addEventListener('click', () => navbar.classList.toggle('active'));
 closeBar.addEventListener('click', () => navbar.classList.toggle('active'));
-navPort.addEventListener('click', () => navbar.classList.toggle('active'));
-navAbout.addEventListener('click', () => navbar.classList.toggle('active'));
-navContact.addEventListener('click', () => navbar.classList.toggle('active'));
+
+
+navPort.addEventListener('click', () => {navbar.classList.toggle('active')
+fixed.classList.toggle('fixed-active');
+});
+navAbout.addEventListener('click', () => {navbar.classList.toggle('active')
+fixed.classList.toggle('fixed-active');
+});
+navContact.addEventListener('click', () => {navbar.classList.toggle('active')
+fixed.classList.toggle('fixed-active');
+});
